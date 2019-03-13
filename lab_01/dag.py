bf1eef885127c3128d6faeec2db66abdfbb04fef
CORP\Ilya.Kobelev <ilya.kobelev@acronis.com>
2019-03-13 21:57:07 +0300

    -

--- lab_01/dag.py
+++ lab_01/dag.py
@@ -1,14 +1,35 @@
-from datetime import datetime
+from datetime import datetime, timedelta, tzinfo
 import elasticsearch
-import csv
-import unicodedata
-from urllib.parse import urlparse
+import json
+import pytz
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
 
-def pull_from_elasticsearch():
+def pull_from_elasticsearch(ds, **kwargs):
     es = elasticsearch.Elasticsearch(["34.76.45.249:9200"])
-    res = es.search(index="ilya.kobelev", body={"query": {"match_all": {}}})
-    sample = res['hits']['hits']
+    res = es.search(index="ilya.kobelev", body={
+        "query": {
+            "range":
+                {"@timestamp": {
+                    "gte": kwargs['execution_date']-timedelta(minutes=15),
+                    "lt": kwargs['execution_date']
+                }
+            }
+        }
+    })
+    with open('query_result.json', 'w') as f:
+        json.dump(res['hits']['hits'], f, ensure_ascii=False)
+
+dag = DAG('lab1_dag', description='Simple tutorial DAG',
+          schedule_interval='0,15,30,45 * * * *',          
+          start_date=datetime(2019, 3, 13, 15, 30, tzinfo=pytz.utc), catchup=False)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+
+pull_from_elasticsearch_operator = PythonOperator(task_id='pull_from_elasticsearch_task', python_callable=pull_from_elasticsearch,provide_context=True, dag=dag)
+
+dummy_operator >> pull_from_elasticsearch_operator
 
 
-pull_from_elasticsearch()
